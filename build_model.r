94406d59a5df82473b4d3a99b9a8d57807eaea47
Javier Rodriguez Zaurin <javier@Javiers-MacBook-Pro.local>
2016-04-19 18:55:20 +0100

    Fun with text

---
+++ build_model.r
@@ -0,0 +1,75 @@
+## to run: Rscript build_model.r dir
+suppressMessages(require(tm))
+suppressMessages(require(SnowballC))
+
+options(mc.cores=1)
+
+args = commandArgs(trailingOnly=TRUE)
+dir <- args[1]
+
+clean.docs <- function(dir, stem = FALSE)
+{
+    ## Function to acess to the documents, clean the content and return a corpus
+    ## ready for processing
+    ##dir: directory containing the docs
+    ##stem: optional stemization
+
+    ## Build a corpus assuming english language
+    fnames <- DirSource(dir,encoding = "UTF-8", recursive=TRUE)
+    corpus <- Corpus(fnames, readerControl=list(reader=readPlain,language="en"))
+
+    ## Map the cleaning task for speed:
+    ## 1- Remove non alphabetical characters
+    corpus <- tm_map(corpus, function(x) gsub("[^[:alpha:]]", " ", x))
+
+    ## 2- all to lower case
+    corpus <- tm_map(corpus, tolower)
+
+    ## 3- Remove Punctuation
+    corpus <- tm_map(corpus, removePunctuation)
+
+    ## 4- Strip WhiteSpaces
+    corpus <- tm_map(corpus, stripWhitespace)
+
+    ## 5- if steming is TRUE, apply stemization
+    if (stem) corpus <- tm_map(corpus, stemDocument)
+
+    ## 6- mapping into adequate format for TermDocumentMatrix
+    corpus <- tm_map(corpus, PlainTextDocument)    
+    
+    return(corpus)
+
+} 
+
+tfidTDM <- function(dir, corpus)
+{
+    
+    ## 1- Grabbing the filenames that will be the column names later for convenience
+    fnames <- DirSource(dir,encoding = "UTF-8", recursive=TRUE)
+    fnames.list <- fnames$filelist
+
+    ## 2- Building term (rows) document (columns) matrix.
+    ##    NOTE: weightTfIdf normalises by default
+    tfidftdm <- TermDocumentMatrix(corpus, control=list(stopwords=TRUE,
+                                                        weighting=weightTfIdf))
+
+    ## 3- Setting colnames to documen names 
+    colnames(tfidftdm) <- fnames.list
+
+    ## 4- removing from the matrix unsually long terms
+    ##    "unusual" is defined as greater than mean(number of characters) + 3*std
+    words <- rownames(tfidftdm)
+    meanchar <- mean(nchar(words))
+    stdchar <- sd(nchar(words))
+    max.numb.chars <- meanchar + 3*stdchar
+    drop.words <- which(nchar(words) > max.numb.chars)
+
+    ## there are only 753 unusal long words to drop
+    tfidftdm <- tfidftdm[-drop.words,]
+    
+    return(tfidftdm)
+}
+
+corpus <- clean.docs(dir)
+tfidftdm <- tfidTDM(dir, corpus)
+save(tfidftdm, file = "model.rda")
---
+++ query.r
@@ -0,0 +1,115 @@
+## to run, for example: Rscript query.r "science and religion or technology" 10 "TRUE"
+suppressMessages(require(tm))
+suppressMessages(require(SnowballC))
+
+args = commandArgs(trailingOnly=TRUE)
+load("model.rda")
+q <- as.character(args[1])
+k <- as.integer(args[2])
+fixed <- as.logical(args[3])
+
+query <- function(q,tfidftdm,k,fixed)
+{
+    ## q        : query
+    ## tfidftdm : TermDocumentMatrix with tf-idf
+    ## k        : number of documents to retrieve
+    ## fixed    : strict (science -> science) or flexible search (science -> science, neuroscience, ...)
+    
+    ## implementing the logic
+    ## 1- splitting the string on "OR" into AND logic blocks
+    or.split <- unlist(strsplit(q, " or "))
+
+    ## 2- group the terms queried via "AND" 
+    terms.list <- sapply(or.split, function(x) strsplit(x, " and "))
+
+    ## 3- Initializing the results vector and searching through every "logic
+    ## block" ( -> "OR")
+    results <- c()
+    for (l in terms.list)
+    {    
+
+        if (fixed){
+            ## find the rows/words in the             
+        rows <- which(rownames(tfidftdm) %in% l)
+        
+        ## if the word is not in the corpus, stop
+        if(length(rows) == 0) stop("fixed Query: some of the inputs words are not in the corpus")            
+        
+        ## list of documents containing the words in the query
+        docs <- list()
+        for (r in rows)
+        {
+            sub.matrix <- as.matrix(tfidftdm[r,])
+            d <- which(sub.matrix != 0)
+            docs <- append(docs, list(d))
+        }
+        
+        ## performing the "AND" logic by intersect the datasets
+        cols <- Reduce(intersect, docs)
+        
+        }
+        ## We add some flexibility to the search by also including documents
+        ## that have words of a similar root than those in the query.
+        ## For example, querying "science" will also consider "neuroscience"
+        else
+        {            
+
+            ## list of documents containing the words in the query
+            docs <- list()
+            for (t in l)
+            {                
+                ## stemming the query-word(s) for flexible search 
+                new.rows <- stemDocument(t)
+
+                ## find the rows/words with "similar" words
+                rows <- which(grepl(new.rows,rownames(tfidftdm)))
+
+                ## if the word is not in the corpus, stop               
+                if(length(rows) == 0) stop("Flexible Query: some of the inputs words are not in the corpus")            
+
+                ## initialise an aggregator of all files with "similar" words
+                agg <- c()
+                for (r in rows)
+                {                    
+                    sub.matrix <- as.matrix(tfidftdm[r,])
+                    d <- which(sub.matrix != 0)
+                    agg <- unique(c(agg,d))
+                    
+                }
+
+                docs <- append(docs,list(agg))
+            } 
+            
+            ## performing the "AND" logic by intersect the datasets
+            cols <- Reduce(intersect, docs)
+        }
+  
+    }
+                       
+    ## 4-finally, adding the tfidf scores...
+    scores <- apply(as.matrix(tfidftdm[rows,cols]), 2, sum)
+    
+    ## adding names to the vector...
+    doc.names <- colnames(tfidftdm)[cols]
+    names(scores) <- doc.names
+    
+    ## aggregating results..
+    results <- c(results,scores)
+
+    ## sorting results based on score, descending order...
+    results <- sort(results, decreasing = TRUE)
+    
+    ## and drop duplicated from the "OR" join and return k documents
+    is.duplicated <- which(duplicated(names(results)))
+    if (any(is.duplicated))   results <- results[-is.duplicated]
+    
+    return(results[1:k])
+        
+}
+
+res <- query(q,tfidftdm,k,fixed)
+out.text <- paste(names(res), as.numeric(res), sep = ": ")
+out.text <- paste(out.text, "\n")
+cat(out.text, "\n")
+
+
