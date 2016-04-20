9c8a574992715391dd62e883ad11919a3752b685
Javier Rodriguez Zaurin <javier@Javiers-MacBook-Pro.local>
2016-04-20 18:52:52 +0100

    More fun with text

--- query.r
+++ query.r
@@ -18,42 +18,43 @@ query <- function(q,tfidftdm,k,fixed)
     ## implementing the logic
     ## 1- splitting the string on "OR" into AND logic blocks
     or.split <- unlist(strsplit(q, " or "))
-
+    
     ## 2- group the terms queried via "AND" 
     terms.list <- sapply(or.split, function(x) strsplit(x, " and "))
-
+    
     ## 3- Initializing the results vector and searching through every "logic
     ## block" ( -> "OR")
     results <- c()
     for (l in terms.list)
     {    
-        # if we should match the query words.
-        if (fixed){
-            ## find the rows/words in the             
-        rows <- which(rownames(tfidftdm) %in% l)
-        
-        ## if the word is not in the corpus, stop
-        if(length(rows) == 0) stop("fixed Query: some of the inputs words are not in the corpus")            
-        
-        ## list of documents containing the words in the query
-        docs <- list()
-        for (r in rows)
+        ## if we should match the query words.
+        if (fixed)
         {
-            sub.matrix <- as.matrix(tfidftdm[r,])
-            d <- which(sub.matrix != 0)
-            docs <- append(docs, list(d))
-        }
-        
-        ## performing the "AND" logic by intersect the datasets
-        cols <- Reduce(intersect, docs)
-        
+            ## find the rows/words in the             
+            rows <- which(rownames(tfidftdm) %in% l)
+            
+            ## if the word is not in the corpus, stop
+            if(length(rows) == 0) stop("fixed Query: some of the inputs words are not in the corpus")            
+            
+            ## list of documents containing the words in the query
+            docs <- list()
+            for (r in rows)
+            {
+                sub.matrix <- as.matrix(tfidftdm[r,])
+                d <- which(sub.matrix != 0)
+                docs <- append(docs, list(d))
+            }
+            
+            ## performing the "AND" logic by intersect the datasets
+            cols <- Reduce(intersect, docs)
+            
         }
         ## We add some flexibility to the search by also including documents
         ## that have words of a similar root than those in the query.
         ## For example, querying "science" will also consider "neuroscience"
         else
         {            
-
+            
             ## list of documents containing the words in the query
             docs <- list()
             for (t in l)
